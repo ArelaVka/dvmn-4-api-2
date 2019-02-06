8d4e721cb3c5c1385461b75bd7365c048a909bef
ArelaVka <arelav90@mail.ru>
2019-02-06 23:39:42 +0300

    fix count_clicks

--- script.py
+++ script.py
@@ -10,17 +10,17 @@ def cut_url(api_token, long_url):
   response_post = requests.post(url_shorten, headers=headers_user, json=json_url)
   return response_post.json().get('id')
 
-def count_clicks(bitlink):
+def count_clicks(api_token, bitlink):
   url_total='https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(bitlink)
-  payload={"unit":"day", "units": "-1"}
-  responce=requests.get(url_total, params=payload)
-  print(responce.url)
-  print(responce.text)
+  payload={"unit":"day"}
+  headers_user = {'Authorization': 'Bearer ' + api_token}
+  responce=requests.get(url_total, headers=headers_user, params=payload)
+  return responce.json()["total_clicks"]
 
 #url=input()
 url='http://ya.ru'
 bitlink=cut_url(token, url)
 if bitlink:
-  count_clicks(bitlink)
+  count_clicks(token, bitlink)
 else:
   print('Your url is incorrect!')
