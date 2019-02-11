a2f6ab4b2bab29f6009bdf841f7f676786d08396
ArelaVka <arelav90@mail.ru>
2019-02-11 23:01:55 +0300

    final - 1

--- script.py
+++ script.py
@@ -1,45 +1,61 @@
 import requests
-import json
+from requests.exceptions import RequestException
+import os
+from dotenv import load_dotenv
 
-token='98c56de89e6e0b5b035ffd02f1f4b1da05e72094'
+load_dotenv() 
+token=os.getenv("TOKEN")
 
 def cut_url(api_token, long_url):
   url_shorten='https://api-ssl.bitly.com/v4/shorten'
   headers_user = {'Authorization': 'Bearer ' + api_token}
   json_url = {'long_url': long_url}
   response_post = requests.post(url_shorten, headers=headers_user, json=json_url)
-  return response_post.json().get('id')
+  return response_post.json().get('id') 
 
 def count_clicks(api_token, bitlink):
   url_total='https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(bitlink)
   payload={"unit":"day"}
   headers_user = {'Authorization': 'Bearer ' + api_token}
   responce=requests.get(url_total, headers=headers_user, params=payload)
-  return responce.json()["total_clicks"]
+  return responce.json()["total_clicks"] 
 
 def check_bitlink(api_token, input_link):
   url_total='https://api-ssl.bitly.com/v4/bitlinks/{}'.format(input_link)
   headers_user = {'Authorization': 'Bearer ' + api_token}
   responce=requests.get(url_total, headers=headers_user)
-  return responce.status_code
+  return responce.ok
 
-#url=input()
-#bitlink=cut_url(token, url)
-#if bitlink:
-#  print(count_clicks(token, bitlink))
-#else:
-#  print('Your url is incorrect!')
+def check_http_bitlink(api_token, input_link):
+  url_total='https://api-ssl.bitly.com/v4/bitlinks/{}'.format(input_link[7:])
+  headers_user = {'Authorization': 'Bearer ' + api_token}
+  responce=requests.get(url_total, headers=headers_user)
+  return responce.ok
 
-#url='http://ya.ru'
-url='http://bit.ly/2t7hCiD'
-print(check_bitlink(token, url))
-#print(count_clicks(token, url))
-if check_bitlink(token, url)==404:
-  print("1")
-  print(cut_url(token, url))
-elif (check_bitlink(token, url))==200:
-  print("2")
-  print('COUNT OF CLICKS: {}'.format(count_clicks(token, url)))
-else:
-  print("3")
-  print('Your url is incorrect!')
+def check_url(input_link):
+  try:
+    responce=requests.get('http://' + url)
+    return responce.ok
+  except RequestException:
+    return None
+
+def check_http_url(input_link):
+  try:
+    responce=requests.get(url)
+    return responce.ok
+  except RequestException:
+    return None
+
+if __name__ == "__main__":
+  url = input('Enter url: ')
+
+  if check_url(url) is None and check_http_url(url) is None:
+    print('Invalid url!')
+  elif check_http_bitlink(token, url):
+    print('The number of clicks on the link : {}'.format(count_clicks(token, url[7:])))
+  elif check_bitlink(token, url):
+    print('The number of clicks on the link: {}'.format(count_clicks(token, url)))
+  elif check_http_url(url):
+    print('Your bitlink: {}'.format(cut_url(token, url)))
+  elif check_url(url):
+    print('Your bitlink: {}'.format(cut_url(token, 'http://' + url)))
